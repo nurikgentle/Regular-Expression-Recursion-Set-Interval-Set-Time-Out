8b14dac704314ead5a2dd2fbe16bd8b58e1ecfd7
nurikgentle <nurikgentle@gmail.com>
2022-07-08 23:54:09 +0600

    this is fucking driving me crazy

---
+++ dz1.js
@@ -0,0 +1,12 @@
+// const input = document.getElementById('input')
+// const button = document.getElementById('button')
+
+// const phoneRegExp = /^\d{0}[\d+]{14}$/;
+
+// button.addEventListener("click", () => {
+//     if(phoneRegExp.test(input.value)){
+//        console.log('Верно');
+//     } else {
+//         console.log('Неверно');
+//     }
+// })
